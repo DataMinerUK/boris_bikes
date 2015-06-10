9d28b1608a8a219c0f870e4154e7c4ee82ee4c30
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-10 15:53:33 +0100

    Adding functionality and tests for a broken bike.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,9 +5,14 @@ describe Bike do
     expect(subject).to respond_to :working?
   end
 
-  it 'can be reprted boken' do
+  it 'can be reported boken' do
     subject.report_broken
     expect(subject).to be_broken
+  end
+
+  it 'does not work when reported broken' do
+    subject.report_broken
+    expect(subject).not_to be_working
   end
 
 end
