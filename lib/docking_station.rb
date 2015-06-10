41b39ba94bf04fc1a8c7795dc14618339fbfd85c
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-10 14:17:08 +0100

    passed test such that docking station has a default capacity of 20

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,17 @@
 #! usr/bin/env ruby
 
 class DockingStation
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    fail 'No bikes found' unless @bike
-    @bike
+    fail 'No bikes found' if @bikes.empty?
+    @bikes.pop
   end
 
   def dock bike
-    fail 'Docking station full' if @bike
-    @bike = bike
+    fail 'Docking station full' if @bikes.count >= 20
+    @bikes << bike
   end
 end
