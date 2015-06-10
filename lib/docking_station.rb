2a42460a15493e05adebc54a408cda8b4847a4c1
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-10 12:02:51 +0100

    added empty release_bike method to docking_stations

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,7 @@
 #! usr/bin/env ruby
 
 class DockingStation
+  def release_bike
 
+  end
 end
--- spec/Docking_station_spec.rb
+++ spec/Docking_station_spec.rb
@@ -6,4 +6,6 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
 
+  
+
 end
