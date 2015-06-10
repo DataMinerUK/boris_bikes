72151a273b31ebae631abeb386a07c81ba5c08d8
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-10 15:20:36 +0100

    code now passes default capacity test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,8 +4,10 @@ class DockingStation
 
   DEFAULT_CAPACITY = 20
 
+  attr_reader :capacity
   def initialize
     @bikes = []
+    @capacity = DEFAULT_CAPACITY
   end
 
   def release_bike
@@ -21,7 +23,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.count >= DEFAULT_CAPACITY
+    @bikes.count >= @capacity
   end
 
   def empty?
