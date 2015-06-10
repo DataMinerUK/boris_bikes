e3c2a3b113c4f7669f05e8e8df0575d6c7904188
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-10 17:59:57 +0100

    Adding test for adding bikes to bike containers.

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -12,6 +12,21 @@ shared_examples_for BikeContainer do
       expect(subject.capacity).to eq random_capacity
     end
   end
+
+  describe 'add_bike' do
+    
+    it 'receives a bike' do
+      subject.add_bike double :bike
+      expect(subject).not_to be_empty
+    end
+
+    it 'raises error when full' do
+      subject.capacity.times{subject.add_bike double(:bike)}
+      expect{subject.add_bike double(:bike)}.to raise_error "#{described_class.name} full"
+    end
+
+  end
+
 end
 
 class BikeContainerTest; include BikeContainer; end
